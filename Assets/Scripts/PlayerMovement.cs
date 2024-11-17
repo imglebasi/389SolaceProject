6021244452611022088b0385bea3a0270ec7ce5e
Isabel <dojofxseito@gmail.com>
2024-11-17 11:28:03 -0800

    two players
    wasd and arrows keys to move em
    they crash into each other and go jiggle
    started camera follow script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed;
+    public bool canMove;
+    public Rigidbody2D rb;
+    private Vector2 playerDirection;
+    public KeyCode[] PlayerKeys;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        foreach(KeyCode key in PlayerKeys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                Debug.Log("this player is pressing their keys!");
+                float directionX = Input.GetAxisRaw("Horizontal");
+                float directionY = Input.GetAxisRaw("Vertical");
+                playerDirection = new Vector2(directionX, directionY).normalized;
+                canMove = true;
+            }
+            else if (Input.GetKeyUp(key))
+            {
+                Debug.Log("this player STOPPED pressing their keys");
+                canMove = false;
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        if (canMove)
+        {
+            rb.velocity = new Vector2(playerDirection.x * playerSpeed, playerDirection.y * playerSpeed);
+        }
+    }
+}
