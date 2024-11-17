8dc5417da67b074986d4408290267b2ec57c9e09
Isabel <dojofxseito@gmail.com>
2024-11-17 11:41:40 -0800

    working on camera follow 2 players
    not workin rn idk ill work on it during work today

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector3 offset;
+    public Vector3 midpos;
+    public GameObject Player1;
+    public GameObject Player2;
+
     void Start()
     {
-        
+        Vector2 midpos = (Player1.transform.position + Player2.transform.position) / 2.0f;
+        offset = transform.position - Player1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        midpos = (Player1.transform.position + Player2.transform.position) / 2.0f;
+
+        transform.position = offset + Player1.transform.position;
     }
 }
